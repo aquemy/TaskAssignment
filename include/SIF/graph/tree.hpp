cd572e47201a556b0f2144d658fbb29701e9ac92
A_Bailly <adeline.bailly@insa-rouen.fr>
2013-04-02 11:58:39 +0200

    Update SIF main header - bis

---
+++ include/SIF/graph/tree.hpp
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// <tree.hpp>
+// Copyright (C), 2013
+//
+// Adeline Bailly, Alexandre Quemy
+//
+// This software is governed by the CeCILL license under French law and
+// abiding by the rules of distribution of free software.  You can  ue,
+// modify and/ or redistribute the software under the terms of the CeCILL
+// license as circulated by CEA, CNRS and INRIA at the following URL
+// "http://www.cecill.info".
+//
+// In this respect, the user's attention is drawn to the risks associated
+// with loading,  using,  modifying and/or developing or reproducing the
+// software by the user in light of its specific status of free software,
+// that may mean  that it is complicated to manipulate,  and  that  also
+// therefore means  that it is reserved for developers  and  experienced
+// professionals having in-depth computer knowledge. Users are therefore
+// encouraged to load and test the software's suitability as regards their
+// requirements in conditions enabling the security of their systems and/or
+// data to be ensured and,  more generally, to use and operate it in the
+// same conditions as regards security.
+// The fact that you are presently reading this means that you have had
+// knowledge of the CeCILL license and that you accept its terms.
+// 
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef _SIF_TREE_
+#define _SIF_TREE_
+
+namespace sif
+{
+
+/** Tree : General API for tree
+
+Tree is the class which list trees.
+
+@see sif::TreeFactory, sif::RTree, sif::QuadTree, sif::AtlasGrid
+*/
+
+class Tree
+{
+
+};
+
+}
+
+#endif // _SIF_TREE_
